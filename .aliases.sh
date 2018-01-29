ec566c008303140d4d4ced480245b48bd615e337
Una Kravets <una.kravets@gmail.com>
2018-01-29 16:03:00 -0500

    updated terminal aliases

--- .aliases.sh
+++ .aliases.sh
@@ -1,6 +1,3 @@
-# tells me my IP Address
-alias myip='curl ip.appspot.com'
-
 # Make directory and enter it
 function mkd() {
     mkdir -p "$@" && cd "$@"
@@ -15,57 +12,9 @@ alias emojis="open http://www.emoji-cheat-sheet.com/"
 # restart window
 alias wtf="source ~/.zshrc"
 
-# edit personal goals
-alias pg-ed="subl3 ~/Desktop/Dev/personal-goals"
-
-# check off personal goals (open main README.md in vim)
-alias pg-ch="vim ~/Desktop/Dev/personal-goals/README.md"
-
-# add a happy moment for the day
-alias add-moment="vim ~/Desktop/Dev/personal-goals/happy-moments/happy-moments.md"
-
-# add to content list (opens content list folder in vim)
-# i.e. pg-add blog-post or pd-add resource
-function pg-add() {
-  if [ $# -eq 0 ]; then
-      print "Oops. Please enter a content type! (i.e. pg-add video)"
-    else
-      vim ~/Desktop/Dev/personal-goals/content-list/"$1"s.md
-  fi
-
-}
-
-# push my changes to my github master branch and open the page
-alias pg-gh="cd ~/Desktop/Dev/personal-goals &&
-  git add -A &&
-  git commit -m 'push from terminal' &&
-  git push origin master &&
-  open http://github.com/una/personal-goals"
-
 # open these directories from anywhere
 alias Dev="~/Desktop/Dev"
-alias Blogs="~/Documents/Blogs"
-
-# create new week in review
-function pg-review() {
-  if [ $# -eq 0 ]; then
-      print "Oops. Please enter a file name: month-week (i.e. 11-week2"
-    else
-      cd ~/Desktop/Dev/personal-goals/accomplishments && touch 2016-"$1".md;
-      cat 0-review-template.md >> 2016-"$1".md && vim 2016-"$1".md;
-  fi
-}
-
-# add a thing to do the the following week
-# this function opens the newest accomplishments file in vim
-# nw stands for "next week"
-function pg-nw() {
-  if [ $# -eq 0 ]; then
-    cd ~/Desktop/Dev/personal-goals/accomplishments;
-    newestFile=$(ls -t | head -n1);
-    vim $newestFile;
-  fi
-}
+alias Bustle="~/Desktop/Dev/Bustle"
 
 # install all the things
 alias start-project="bundle install && bower install && npm install"
@@ -104,3 +53,20 @@ function get-audio() {
     youtube-dl --extract-audio --audio-format mp3 $1
   fi
 }
+
+# Removes all branches that have already been merged to master (or dev)
+alias clean-branches='git branch --merged | egrep -v "(^\*        |master|dev)" | xargs git branch -d'
+
+# Removes node modules and reinstalls with yarn
+alias clean_start="rm -rf node_modules && yarn install && yarn start"
+
+# Quick Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+# Gets IP
+alias myip="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+#Away from keyboard :smoking:
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
